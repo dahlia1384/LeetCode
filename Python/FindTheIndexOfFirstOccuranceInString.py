309dc6f3424f87ec8464d0a1d26b7a20810324fd
Dahlia Mohammadi <mohammadi.dahlia@gmail.com>
2025-07-06 11:01:34 -0700

    07.06.2025

---
+++ Python/FindTheIndexOfFirstOccuranceInString.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if not needle:
+            return 0
+
+        needle_len = len(needle)
+        haystack_len = len(haystack)
+
+        for i in range(haystack_len - needle_len + 1):
+            if haystack[i:i+needle_len] == needle:
+                return i
+
+        return -1
